a781bf026e96a02979fa732af89fd78dadaecfde
wjm41 <wjm41@cam.ac.uk>
2022-05-25 10:12:52 +0100

    fixing paths

--- on2logic/case_study.py
+++ on2logic/case_study.py
@@ -11,7 +11,7 @@ def process_case_study_items(data_dir):
     df_items = pd.read_csv(path_to_case_study_items, header=None, names=['item_ids'])
     
     for item in df_items.item_ids:
-        process_item_name(item, data_dir=data_dir)
+        process_item_name(item, data_dir=f'{data_dir}/images/cudl')
     
     return
 
